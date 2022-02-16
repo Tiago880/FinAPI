1d0a13911b7ee993655846936bd0d1b0219711b2
Tiago880 <tiagocruz_880@hotmail.com>
2022-02-16 14:32:21 -0300

    FinAPI

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 //Verificar se é uma conta existente
+const { request } = require("express");
 const express = require("express");
 const app = express();
 
@@ -17,7 +18,7 @@ function verifyExistsAccountCPF(request, response, next) {
         cpf
     } = request.headers;
 
-    const customer = customers.find(customer => customer.cpf === cpf);
+    const customer = customers.find((customer) => customer.cpf === cpf);
 
     if (!customer) {
         return response.status(400).json({
@@ -26,6 +27,18 @@ function verifyExistsAccountCPF(request, response, next) {
     }
     request.customer = customer;
     return next();
+}
+
+
+function getBalance(statement){
+    const balance = statement.reduce((acc, operation)=>{
+        if(operation.type === 'credit') {
+            return acc + operation.amount;
+            } else {
+            return acc - operation.amount;
+        }
+    }, 0);
+    return balance;
 }
 
 app.post("/account", (request, response) => {
@@ -65,4 +78,91 @@ app.get("/statement", verifyExistsAccountCPF, (request, response) => {
     return response.json(customer.statement);
 
 });
+
+app.post("/deposit",verifyExistsAccountCPF, (request, response) => {
+
+const {description, amount} = request.body;
+
+const {customer} = request;
+
+const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit"
+}
+    customer.statement.push(statementOperation);
+    return response.status(201).send();
+});
+
+app.post("/withdraw", verifyExistsAccountCPF , (request,response) => {
+
+    const { amount } = request.body;
+    
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount ){
+        return response.status(400).json({error: "Dinheiro Insuficiência"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    };
+
+    customer.statement.push(statementOperation);
+    return response.status(201).send();
+});
+
+
+    app.get("/statement/date", verifyExistsAccountCPF,(request, response) => {
+        const {customer} = request;
+        const { date } = request.query;
+
+        const dateFormat = new Date(date + " 00:00");
+
+        const statement = customer.statement.filter((statement) => 
+        
+        statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+        
+        return response.json(customer.statement);
+    });
+
+
+    app.put("/account",verifyExistsAccountCPF,(request, response) => {
+        const {name} = request.body;
+        const {customer} = request;
+
+        customer.name = name;
+        
+        return response.status(201).send();
+    });
+
+    app.get("/account", verifyExistsAccountCPF, (request, response) => {
+        
+        const {customer} = request;
+        return response.json(customer);
+
+
+    });
+
+    app.delete("/account", verifyExistsAccountCPF, (request, response) => {
+        const {customer} = request;
+         
+        customers.splice(customer,1);
+
+        return response.status(200).json(customers);
+
+    });
+
+    app.get("/balance", verifyExistsAccountCPF, (request, response) => {
+        const {customer} = request;
+
+        const balance = getBalance(customer.statement);
+
+        return response.json(balance);
+    }); 
 app.listen(3333);
